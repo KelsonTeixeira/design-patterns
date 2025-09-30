29be45b2664a84d631f4bedc0d292f645e9e7384
kelsonteixeira <txkelson@gmail.com>
2025-09-30 02:19:25 -0300

    singleton pattern

---
+++ singleton/fancyLogger.js
@@ -0,0 +1,23 @@
+class FancyLogger {
+  constructor () {
+    if(!FancyLogger.instance) {
+      this.logs = [];
+      FancyLogger.instance = this;
+    }
+    return FancyLogger.instance;
+  }
+
+  log (message) {
+    this.logs.push(message);
+    console.log(`Fancy: ${message}`);
+  }
+
+  printLogCount() {
+    console.log(`${this.logs.length} Logs!`);
+  }
+};
+
+const logger = new FancyLogger;
+Object.freeze(logger);
+
+export default logger;
---
+++ singleton/firstUse.js
@@ -0,0 +1,7 @@
+import logger from "./fancyLogger.js";
+
+export default function logFirstImplementation() {
+  logger.printLogCount();
+  logger.log('first use of log');
+  logger.log('first use of log 2');
+};
